638a58987a6193032d4571c771b4177e28e64ee8
heyrict <xiezh0831@126.com>
2017-06-12 12:09:11 +0800

    add include/exclude and space options

--- symexchange.py
+++ symexchange.py
@@ -1,15 +1,47 @@
 #!/usr/bin/env python3
 
 import optparse
-from data_processing import split_wrd
+from data_processing import split_wrd, auto_newline
 
-HALF = list('*,.?!()[];:\'\'\"\"')
-FULL = list('×，。？！（）【】；：‘’“”')
+HALFFULL=[('*','×'),(',','，'),('.','。'),('.','．'),\
+         ('?','？'),('!','！'),('(','（'),(')','）'),\
+         ('[','【'),(']','】'),(';','；'),('\'','‘'),\
+         ('\'','’'),('\"','“'),('\"','”'),]
 
 def main():
+    global HALFFULL
     opt = optparse.OptionParser()
+    opt.set_usage(auto_newline('%%prog will replace the symbols in SBC case(full) to '\
+            'DBC case(half) or the reverse(by using -u, --to-full option. '\
+            '%%prog has a preset DBC cases of %s '\
+            'and you can adapt it to your preferences by using '\
+            '-a and -x options.'%[i[1] for i in HALFFULL]))
     opt.add_option('-u','--to-full',dest='to_full',action='store_true',default=False)
+    opt.add_option('-x',dest='exclude',help='place `half` and `full` symbols '\
+            'to exclude separated by a space',action='append',default=[])
+    opt.add_option('-a',dest='include',help='place `half` and `full` symbols '\
+            'to include separated by a space',action='append',default=[])
+    opt.add_option('-s','--space',dest='space',help='add space after each replacement'\
+            '(doesn\'t work if -u/--to-full is set',action='store_true',default=False)
     (options,args) = opt.parse_args()
+
+    options.include = [i.split(' ') for i in options.include]
+    options.exclude = [i.split(' ') for i in options.exclude]
+
+    for l in [options.include, options.exclude]:
+        if len(l) == 0: continue
+        temp = [len(i) for i in l]
+        if (max(temp) != min(temp)) | (max(temp) != 2):
+            print('ERROR: the value of include/exclude is invalid')
+            exit(1)
+
+    for i in options.exclude:
+        try: HALFFULL.remove(i)
+        except: print('WARNING:',i,'is not in preset cases')
+    HALFFULL += options.include
+
+    HALF = [i[0] + ' '*options.space for i in HALFFULL]
+    FULL = [i[1] for i in HALFFULL]
 
     for i in args:
         with open(i) as f:
