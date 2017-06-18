e5866453015773f1e1d229289ff4767f38452067
heyrict <xiezh0831@126.com>
2017-06-18 17:25:00 +0800

    `only` option added for preferences

--- symexchange.py
+++ symexchange.py
@@ -23,25 +23,32 @@ def main():
             'to include separated by a space',action='append',default=[])
     opt.add_option('-s','--space',dest='space',help='add space after each replacement'\
             '(doesn\'t work if -u/--to-full is set',action='store_true',default=False)
+    opt.add_option('-o','--only',dest='only',help='to contain only the given rules'\
+            'separated by a space',action='append',default=[])
     (options,args) = opt.parse_args()
 
     options.include = [i.split(' ') for i in options.include]
     options.exclude = [i.split(' ') for i in options.exclude]
+    options.only = [i.split(' ') for i in options.only]
 
-    for l in [options.include, options.exclude]:
+    for l in [options.include, options.exclude, options.only]:
         if len(l) == 0: continue
         temp = [len(i) for i in l]
         if (max(temp) != min(temp)) | (max(temp) != 2):
-            print('ERROR: the value of include/exclude is invalid')
+            print('ERROR: the value of include/exclude/only is invalid')
             exit(1)
 
-    for i in options.exclude:
-        try: HALFFULL.remove(i)
-        except: print('WARNING:',i,'is not in preset cases')
-    HALFFULL += options.include
+    if options.only:
+        HALF = [i[0] + ' '*options.space for i in options.only]
+        FULL = [i[1] for i in options.only]
+    else:
+        for i in options.exclude:
+            try: HALFFULL.remove(i)
+            except: print('WARNING:',i,'is not in preset cases')
+        HALFFULL += options.include
 
-    HALF = [i[0] + ' '*options.space for i in HALFFULL]
-    FULL = [i[1] for i in HALFFULL]
+        HALF = [i[0] + ' '*options.space for i in HALFFULL]
+        FULL = [i[1] for i in HALFFULL]
 
     for i in args:
         with open(i) as f:
