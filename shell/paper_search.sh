37860a3f703b6cc731b719a5c9a1bee51184e12e
heyrict <xiezh0831@yahoo.co.jp>
2021-02-26 15:20:35 +0800

    fix: Open webpages with firefox

--- shell/paper_search.sh
+++ shell/paper_search.sh
@@ -1,51 +1,51 @@
 ## paper search engine
-alias webofscience="browse http://apps.webofknowledge.com/"
-alias sciencedirect="browse http://www.sciencedirect.com/"
+alias webofscience="firefox http://apps.webofknowledge.com/"
+alias sciencedirect="firefox http://www.sciencedirect.com/"
 
 pubmed(){
     if test $# -eq 0
     then
-        browse https://www.ncbi.nlm.nih.gov/pubmed
+        firefox https://www.ncbi.nlm.nih.gov/pubmed
     else
         local str=""
         for i in $*;do str=$str"+"$i;done
-        browse https://www.ncbi.nlm.nih.gov/pubmed?term=${str:1}
+        firefox https://www.ncbi.nlm.nih.gov/pubmed?term=${str:1}
     fi
 }
 
 pmid(){
-    browse https://www.ncbi.nlm.nih.gov/pubmed/$1
+    firefox https://www.ncbi.nlm.nih.gov/pubmed/$1
 }
 
 ncbi(){
     if test $# -eq 0
     then
-        browse https://www.ncbi.nlm.nih.gov/
+        firefox https://www.ncbi.nlm.nih.gov/
     else
         local str=""
         for i in $*;do str=$str"+"$i;done
-        browse https://www.ncbi.nlm.nih.gov/gquery/?term=${str:1}
+        firefox https://www.ncbi.nlm.nih.gov/gquery/?term=${str:1}
     fi
 }
 
 nature(){
     if test $# -eq 0
     then
-        browse https://www.nature.com/
+        firefox https://www.nature.com/
     else
         local str=""
         for i in $*;do str=$str"+"$i;done
-        browse https://www.nature.com/search?q=${str:1}
+        firefox https://www.nature.com/search?q=${str:1}
     fi
 }
 
 wanfang(){
     if test $# -eq 0
     then
-        browse http://s.g.wanfangdata.com.cn
+        firefox http://s.g.wanfangdata.com.cn
     else
         local str=""
         for i in $*;do str=$str"+"$i;done
-        browse http://s.g.wanfangdata.com.cn/Paper.aspx?q=${str:1}
+        firefox http://s.g.wanfangdata.com.cn/Paper.aspx?q=${str:1}
     fi
 }
